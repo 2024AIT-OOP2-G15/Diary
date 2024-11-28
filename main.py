8ac321743eb02d582e9c29925f115b45c894e38f
ryochacha <ryoei.y.06.26@gmail.com>
2024-11-28 15:25:53 +0900

    add DiaryRyoei

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.DiaryRyoei import DiaryRyoei
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ] 
+diaries = [DiarySample(),
+           DiaryRyoei(),
+           ]
 
 for d in diaries:
     print("---------------------------------")
