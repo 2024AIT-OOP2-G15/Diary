29c68b3feb2d302fec5bfe973e8b27fe700d2df7
1226-yuya <1226yukao@gmail.com>
2024-11-28 15:50:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from diaries.DiarySample import DiarySample
-
+from diaries.yuyaDiary import yuyaDiary
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ] 
+diaries = [DiarySample(), yuyaDiary()] 
 
 for d in diaries:
     print("---------------------------------")
